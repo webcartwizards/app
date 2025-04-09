bad121dcd4ce6b5bb023ea0110eae6e8e0ab8fa2
rowel127 <rowel.espejo.044@my.csun.edu>
2025-04-09 16:34:06 -0700

    updated Cart class to enable to use of removeFromCart with multiple items

--- webcartwiz/src/main/backend/com/ecommerce/Cart.java
+++ webcartwiz/src/main/backend/com/ecommerce/Cart.java
@@ -20,13 +20,13 @@ public class Cart {
             System.out.println("You cannot remove more than the quantity");
             return false;
         } else {
+            item.quantity = item.quantity - num;
             for(int i = 0; i < CartList.size(); i++){
                 if(CartList.get(i) == item && num > 0){
                     CartList.remove(i);
                     num--;
                 }
             }
-            item.quantity = item.quantity - num;
             return true;
         }
     }
