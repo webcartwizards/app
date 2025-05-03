50b97513a47144e71ad009cb61c77dfc0b28986b
rowel127 <rowel.espejo.044@my.csun.edu>
2025-05-02 21:22:21 -0700

    LoginControllerTest started

---
+++ src/test/LoginControllerTest.java
@@ -0,0 +1,12 @@
+
+import com.ecommerce.Customer;
+import com.ecommerce.LoginController;
+import org.junit.jupiter.api.*;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class LoginControllerTest {
+}
